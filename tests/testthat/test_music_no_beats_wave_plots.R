f25d0a1d825483a172a4c738cd923669d61245f5
Brian Mulloy <brian@landlessness.net>
2024-11-25 15:55:50 -0500

    about to start on the hesien signal refactor

--- tests/testthat/test_music_no_beats_wave_plots.R
+++ tests/testthat/test_music_no_beats_wave_plots.R
@@ -19,3 +19,57 @@ test_that("wave plot for each interval matches snapshot", {
     )
   })
 })
+
+# Define test cases for each interval using vdiffr::expect_doppelganger
+test_that("M3 Time Signal", {
+
+  M3_spectrum = framed_intervals$Major3 %>% frequency_spectrum_for(num_harmonics = 2)
+
+  # Create the signal object from the spectrum
+  signal_obj <- time_signal(M3_spectrum)
+
+  expect_equal(signal_obj$plot_color, colors_homey$major)
+
+  # Define label and coordinate range
+  label <- "M3 Time Signal"
+
+  # Use vdiffr to capture and test the plot output
+  vdiffr::expect_doppelganger(label, function() plot_details.signal(signal_obj, resolution = 2000))
+
+})
+
+# Define test cases for each interval using vdiffr::expect_doppelganger
+test_that("M3 Time Heisen Signal", {
+
+  M3_spectrum = framed_intervals$Major3 %>% frequency_spectrum_for(num_harmonics = 2)
+
+  # Create the signal object from the spectrum
+  signal_obj <- time_heisen_signal(M3_spectrum)
+
+  expect_equal(signal_obj$plot_color, colors_homey$major)
+
+  # Define label and coordinate range
+  label <- "M3 Time Signal"
+
+  # Use vdiffr to capture and test the plot output
+  vdiffr::expect_doppelganger(label, function() plot_details.signal(signal_obj, resolution = 2000))
+
+})
+
+# Define test cases for each interval using vdiffr::expect_doppelganger
+test_that("m6 Time Signal", {
+
+  m6_spectrum = framed_intervals$minor6 %>% frequency_spectrum_for(num_harmonics = 2)
+
+  # Create the signal object from the spectrum
+  signal_obj <- time_signal(m6_spectrum)
+
+  expect_equal(signal_obj$plot_color, colors_homey$major)
+
+  # Define label and coordinate range
+  label <- "m6 Time Signal"
+
+  # Use vdiffr to capture and test the plot output
+  vdiffr::expect_doppelganger(label, function() plot_details.signal(signal_obj, resolution = 2000))
+
+})
