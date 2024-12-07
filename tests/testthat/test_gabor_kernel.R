0362c02de5e3db300c7b98f1d358d6e87ece2b96
Brian Mulloy <brian@landlessness.net>
2024-12-07 09:58:57 -0500

    DRY test for gabor kernel

--- tests/testthat/test_gabor_kernel.R
+++ tests/testthat/test_gabor_kernel.R
@@ -1,58 +1,74 @@
 source(testthat::test_path("helper.R"))
 
+# Helper function to load and preprocess an image
+load_and_preprocess_image <- function(image_file_path) {
+  original <- imager::load.image(image_file_path)
+  grayscale_image <- imager::grayscale(original)
+  list(
+    original_dim = dim(original),
+    grayscale_matrix = as.matrix(grayscale_image)
+  )
+}
+
+# Helper function to test and visualize a Gabor kernel
+test_gabor_kernel <- function(label_prefix, kernel, suffix = "", fft_shift = FALSE, magnitude = FALSE, log_scaling = FALSE) {
+  label <- paste(label_prefix, suffix)
+  vdiffr::expect_doppelganger(
+    label,
+    plot_matrix(kernel, fft_shift = fft_shift, magnitude = magnitude, log_scaling = log_scaling)
+  )
+}
+
+# Helper function to apply Gabor filter and visualize the result
+test_gabor_filter <- function(label_prefix, grayscale_matrix, kernel, suffix = "") {
+  response <- imager::convolve(imager::as.cimg(grayscale_matrix), imager::as.cimg(Re(kernel))) +
+    1i * imager::convolve(imager::as.cimg(grayscale_matrix), imager::as.cimg(Im(kernel)))
+  filtered_image <- imager::as.cimg(Mod(response), dim = dim(grayscale_matrix))
+  label <- paste(label_prefix, suffix)
+  vdiffr::expect_doppelganger(
+    label,
+    plot(filtered_image, axes = FALSE)
+  )
+}
+
+# Helper function to compute and visualize Gabor spectrum
+test_gabor_spectrum <- function(label_prefix, kernel, suffix = "") {
+  spectrum <- fftwtools::fftw2d(kernel)
+  label <- paste(label_prefix, suffix)
+  vdiffr::expect_doppelganger(label, plot_matrix(spectrum))
+  spectrum
+}
+
 test_that('gabor filters with varied parameters', {
+  params <- list(
+    kernel_sizes = c(31),
+    gammas = c(0.8),
+    etas = c(0.8),
+    frequencies = c(0.1),
+    phases = c(0),
+    orientations = c(0, 1/4, 1/2, 3/4) * pi
+  )
 
-  kernel_sizes <- c(31)  # Try different kernel sizes
-  gammas <- c(0.8)  # Aspect ratios in x
-  etas <- c(0.8)  # Aspect ratios in y
-  frequencies <- c(0.1)  # Different sinusoidal densities
-  phases <- c(0)  # Phase offsets
-  orientations <- c(0, 1/4, 1/2, 3/4) * pi
+  image_data <- load_and_preprocess_image(test_path("images", "Lenna.png"))
+  grayscale_matrix <- image_data$grayscale_matrix
 
-  image_file_path <- test_path("images", "Lenna.png")
-  expected_original <- imager::load.image(image_file_path)
-  expected_original_dim <- dim(expected_original)
-  expected_grayscale_image = imager::grayscale(expected_original)
-  expected_grayscale_image_matrix = as.matrix(expected_grayscale_image)
+  for (kernel_size in params$kernel_sizes) {
+    for (gamma in params$gammas) {
+      for (eta in params$etas) {
+        for (frequency in params$frequencies) {
+          for (phase in params$phases) {
+            for (orientation in params$orientations) {
+              suffix <- paste(kernel_size, gamma, eta, frequency, phase, orientation / pi)
 
-  for (kernel_size in kernel_sizes) {
-    for (gamma in gammas) {
-      for (eta in etas) {
-        for (f in frequencies) {
-          for (psi in phases) {
-            for (orientation in orientations) {
+              gabor_kernel <- create_gabor_kernel(kernel_size, gamma, eta, orientation, frequency, phase)
+              test_gabor_kernel('orig kernel', gabor_kernel, suffix)
 
-              label = paste('orig kernel', kernel_size, gamma, eta, f, psi, orientation / pi)
-              gabor_kernel <- create_gabor_kernel(kernel_size, gamma, eta, orientation, f, psi)
-              vdiffr::expect_doppelganger(
-                label,
-                plot_matrix(gabor_kernel, fft_shift = F, magnitude = F, log_scaling = F)
-              )
+              test_gabor_filter('Lenna Gabor Filtered', grayscale_matrix, gabor_kernel, suffix)
 
-              label = paste('Lenna Gabor Filtered', kernel_size, gamma, eta, f, psi, orientation / pi)
-              gabor_filtered_image = apply_gabor_filter(expected_grayscale_image_matrix,
-                                                        orientation,
-                                                        f,
-                                                        kernel_size)
-              vdiffr::expect_doppelganger(
-                label,
-                plot(gabor_filtered_image, axes = F)
-              )
+              spectrum <- test_gabor_spectrum('kernel spectrum', gabor_kernel, suffix)
 
-              label = paste('kernel spectrum', kernel_size, gamma, eta, f, psi, orientation / pi)
-              gabor_spectrum = fftwtools::fftw2d(gabor_kernel)
-              vdiffr::expect_doppelganger(
-                label,
-                plot_matrix(gabor_spectrum)
-              )
-
-              label = paste('reconstructed kernel', kernel_size, gamma, eta, f, psi, orientation / pi)
-              gabor_signal <- fftwtools::fftw2d(gabor_spectrum, inverse = 1)
-              vdiffr::expect_doppelganger(
-                label,
-                plot_matrix(gabor_signal, fft_shift = F, magnitude = F, log_scaling = F)
-              )
-
+              reconstructed <- fftwtools::fftw2d(spectrum, inverse = 1)
+              test_gabor_kernel('reconstructed kernel', reconstructed, suffix)
             }
           }
         }
@@ -62,130 +78,28 @@ test_that('gabor filters with varied parameters', {
 })
 
 test_that('gabor filters with combined parameters', {
-
-  kernel_size = 31
-  gamma = 0.8
-  eta = 0.8
-  frequency = 0.1
-  phase = 0
+  kernel_size <- 31
+  gamma <- 0.8
+  eta <- 0.8
+  frequency <- 0.1
+  phase <- 0
   orientations <- c(0, 1/4, 1/2, 3/4) * pi
 
-  image_file_path <- test_path("images", "Lenna.png")
-  expected_original <- imager::load.image(image_file_path)
-  expected_original_dim <- dim(expected_original)
-  expected_grayscale_image = imager::grayscale(expected_original)
-  expected_grayscale_image_matrix = as.matrix(expected_grayscale_image)
-
-  combined_gabor_kernel = matrix(0 + 0i, nrow = kernel_size, ncol = kernel_size)
-
+  combined_kernel <- matrix(0 + 0i, nrow = kernel_size, ncol = kernel_size)
   for (orientation in orientations) {
-
-    gabor_kernel <- create_gabor_kernel(kernel_size, gamma, eta, orientation, frequency, psi)
-    combined_gabor_kernel =  combined_gabor_kernel + gabor_kernel
+    combined_kernel <- combined_kernel + create_gabor_kernel(kernel_size, gamma, eta, orientation, frequency, phase)
   }
 
-  label = paste('combined kernel')
-  vdiffr::expect_doppelganger(
-    label,
-    plot_matrix(combined_gabor_kernel, fft_shift = F, magnitude = F, log_scaling = F)
-  )
+  test_gabor_kernel('combined kernel', combined_kernel)
 
-  label = paste('Combined Lenna Gabor Filtered')
-  # Perform convolution with real and imaginary components
-  response <- imager::convolve(imager::as.cimg(expected_grayscale_image_matrix), imager::as.cimg(Re(combined_gabor_kernel))) +
-    1i * imager::convolve(imager::as.cimg(expected_grayscale_image_matrix), imager::as.cimg(Im(combined_gabor_kernel)))
+  lenna_data <- load_and_preprocess_image(test_path("images", "Lenna.png"))
+  test_gabor_filter('Combined Lenna Gabor Filtered', lenna_data$grayscale_matrix, combined_kernel)
 
-  # Return the magnitude of the response as an image
-  gabor_filtered_image = imager::as.cimg(Mod(response), dim = dim(grayscale_matrix))
+  madukes_data <- load_and_preprocess_image(test_path("images", "MaDukes.png"))
+  test_gabor_filter('Combined Ma Dukes Gabor Filtered', madukes_data$grayscale_matrix, combined_kernel)
 
-  vdiffr::expect_doppelganger(
-    label,
-    plot(gabor_filtered_image, axes = F)
-  )
+  combined_spectrum <- test_gabor_spectrum('combined kernel spectrum', combined_kernel)
 
-  label = paste('combined kernel spectrum')
-  combined_gabor_spectrum = fftwtools::fftw2d(combined_gabor_kernel)
-  vdiffr::expect_doppelganger(
-    label,
-    plot_matrix(combined_gabor_spectrum)
-  )
-
-  label = paste('combined reconstructed kernel')
-  combined_gabor_signal <- fftwtools::fftw2d(combined_gabor_spectrum, inverse = 1)
-  vdiffr::expect_doppelganger(
-    label,
-    plot_matrix(combined_gabor_signal, fft_shift = F, magnitude = F, log_scaling = F)
-  )
-
-})
-
-test_that('gabor filters with combined parameters', {
-
-  kernel_size = 31
-  gamma = 0.8
-  eta = 0.8
-  frequency = 0.1
-  phase = 0
-  orientations <- c(0, 1/4, 1/2, 3/4) * pi
-
-  combined_gabor_kernel = matrix(0 + 0i, nrow = kernel_size, ncol = kernel_size)
-
-  for (orientation in orientations) {
-
-    gabor_kernel <- create_gabor_kernel(kernel_size, gamma, eta, orientation, frequency, psi)
-    combined_gabor_kernel =  combined_gabor_kernel + gabor_kernel
-  }
-
-  label = paste('combined kernel')
-  vdiffr::expect_doppelganger(
-    label,
-    plot_matrix(combined_gabor_kernel, fft_shift = F, magnitude = F, log_scaling = F)
-  )
-
-  label = paste('Combined Lenna Gabor Filtered')
-  image_file_path <- test_path("images", "Lenna.png")
-  expected_original <- imager::load.image(image_file_path)
-  expected_original_dim <- dim(expected_original)
-  expected_grayscale_image = imager::grayscale(expected_original)
-  expected_grayscale_image_matrix = as.matrix(expected_grayscale_image)
-  # Perform convolution with real and imaginary components
-  response <- imager::convolve(imager::as.cimg(expected_grayscale_image_matrix), imager::as.cimg(Re(combined_gabor_kernel))) +
-    1i * imager::convolve(imager::as.cimg(expected_grayscale_image_matrix), imager::as.cimg(Im(combined_gabor_kernel)))
-  # Return the magnitude of the response as an image
-  gabor_filtered_image = imager::as.cimg(Mod(response), dim = dim(grayscale_matrix))
-  vdiffr::expect_doppelganger(
-    label,
-    plot(gabor_filtered_image, axes = F)
-  )
-
-  label = paste('Combined Ma Dukes Gabor Filtered')
-  image_file_path <- test_path("images", "MaDukes.png")
-  expected_original <- imager::load.image(image_file_path)
-  expected_original_dim <- dim(expected_original)
-  expected_grayscale_image = imager::grayscale(expected_original)
-  expected_grayscale_image_matrix = as.matrix(expected_grayscale_image)
-  # Perform convolution with real and imaginary components
-  response <- imager::convolve(imager::as.cimg(expected_grayscale_image_matrix), imager::as.cimg(Re(combined_gabor_kernel))) +
-    1i * imager::convolve(imager::as.cimg(expected_grayscale_image_matrix), imager::as.cimg(Im(combined_gabor_kernel)))
-  # Return the magnitude of the response as an image
-  gabor_filtered_image = imager::as.cimg(Mod(response), dim = dim(grayscale_matrix))
-  vdiffr::expect_doppelganger(
-    label,
-    plot(gabor_filtered_image, axes = F)
-  )
-
-  label = paste('combined kernel spectrum')
-  combined_gabor_spectrum = fftwtools::fftw2d(combined_gabor_kernel)
-  vdiffr::expect_doppelganger(
-    label,
-    plot_matrix(combined_gabor_spectrum)
-  )
-
-  label = paste('combined reconstructed kernel')
-  combined_gabor_signal <- fftwtools::fftw2d(combined_gabor_spectrum, inverse = 1)
-  vdiffr::expect_doppelganger(
-    label,
-    plot_matrix(combined_gabor_signal, fft_shift = F, magnitude = F, log_scaling = F)
-  )
-
+  reconstructed_combined <- fftwtools::fftw2d(combined_spectrum, inverse = 1)
+  test_gabor_kernel('combined reconstructed kernel', reconstructed_combined)
 })
